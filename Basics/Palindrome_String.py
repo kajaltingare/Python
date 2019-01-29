9236a4fd6bfc084834a48b4d4a0966c20c2453c8
kajal <kajalt.23394@gmail.com>
2019-01-29 18:37:26 +0530

    Create Palindrome_String.py

---
+++ Basics/Palindrome_String.py
@@ -0,0 +1,13 @@
+def isStrPalindrome(inp_str):
+	if(inp_str[::]==inp_str[::-1]):
+	    return True
+	else:
+	    return False
+def main():
+    inp_str = input('Enter the string to check palindrome for: ')
+    if(isStrPalindrome(inp_str)):
+        print("Palindrome found!")
+    else:
+        print("No palindrome.")
+if __name__=='__main__':
+    main()
