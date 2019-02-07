3709151d497660b8f1d7d4c8efc40fb90d42c902
kajal <kajalt.23394@gmail.com>
2019-02-07 22:59:56 +0530

    Create turnOnBits.py

---
+++ Basics/UsingFunc/turnOnBits.py
@@ -0,0 +1,13 @@
+def turnOnBits(no,pos,bits):
+    x = (1<<bits) - 1
+    x = x<<(pos-bits)
+    result = no | x
+    return result
+
+def main():
+    no,pos,bits = eval(input('Enter the no,position,no of bits to turn on: '))
+    result = turnOnBits(no,pos,bits)
+    print('Number after turning off bit: %d'%result)
+    
+if __name__ == '__main__':
+    main()
