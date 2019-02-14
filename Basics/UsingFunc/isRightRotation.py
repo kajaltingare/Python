d3f75b57adf3de3eee1c7e0464667990f1882dc0
kajal <kajalt.23394@gmail.com>
2019-02-14 21:30:21 +0530

    Add files via upload

---
+++ Basics/UsingFunc/isRightRotation.py
@@ -0,0 +1,19 @@
+def isRightRotation(firstString, secondString):
+    completeStr =  secondString*2
+    if(firstString in completeStr):
+        return True
+    else:
+        return False
+
+def main():
+    firstString = eval(input('Enter the first string: '))
+    secondString = eval(input('Enter the second string: '))
+    result = isRightRotation(firstString, secondString)
+    if(result):
+        print('%s is right rotation of %s'%(firstString,secondString))
+    else:
+        print('No right rotation found.')
+
+if __name__ == '__main__':
+    main()
+
---
+++ Basics/UsingFunc/tablePowerInTwos.py
@@ -0,0 +1,62 @@
+def tablePowerInTwos(num):
+    for temp in range(1,11):
+        if(temp==1):
+            one = '0001'
+            printBinary(one,num)
+        elif(temp==2):
+            two = '0010'
+            printBinary(two,num)
+        elif(temp==3):
+            three = '0011'
+            printBinary(three,num)
+        elif(temp==4):
+            four = '0100'
+            printBinary(four,num)
+        elif(temp==5):
+            five = '0101'
+            printBinary(five,num)
+        elif(temp==6):
+            six = '0110'
+            printBinary(six,num)
+        elif(temp==7):
+            seven = '0111'
+            printBinary(seven,num)
+        elif(temp==8):
+            eight = '1000'
+            printBinary(eight,num)
+        elif(temp==9):
+            nine = '1001'
+            printBinary(nine,num)
+        elif(temp==10):
+            ten = '1010'
+            printBinary(ten,num)
+
+def printBinary(bits,num):
+    temp = num
+    loopCnt = 0
+    while(temp!=1):
+        temp = temp/2
+        loopCnt+=1
+    for _ in range(0,loopCnt):
+        bits = bits + '0'
+    print(bits)
+
+def countNoofOne(n):
+    count = 0
+    while(n!=0):
+        count+=1
+        n = n &(n-1)
+    return count
+
+def main():
+    num = eval(input('Enter any number in 2\'s power to print it\'s table: '))
+    if(num==1):
+        print('Please enter valid number.')
+    elif (countNoofOne(num)==1):
+            print('Table in binary for number %d'%num)
+            tablePowerInTwos(num)
+    else:
+        print('Please enter valid number.')
+
+if __name__ == '__main__':
+    main()
