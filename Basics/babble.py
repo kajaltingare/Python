e1dde0a980eaa688f13b2508cecffc7d295ba99f
kajal <kajalt.23394@gmail.com>
2019-02-16 19:21:32 +0530

    Update babble.py

--- Basics/babble.py
+++ Basics/babble.py
@@ -1,18 +1,5 @@
-name=str(input('Enter the string: ')) #babble
+# Write a program to accept a string from user, replace all occurances of 1st character with * in remaining string.
+name = str(input('Enter the string: ')) #babble
 print("You entered the string as {0}".format(name))
 print("Now, first character is {0} and replaced next all occurences by * in given string as: ".format(name[0]))
-#print(name.replace("b","*")) op: *a**le
-
-firstChar = name[0:1]
-#print(firstChar)  op:b
-
-remained = name[1:]
-#print(remained)  op:abble
-
-print(firstChar+remained.replace("b","*")) #ba**le
-
-'''
->>> s='babble'
->>> print(s[0]+s[1:].replace(s[0],'*'))
-ba**le
->>> '''
+print(name[0:1]+name[1:].replace('b','*'))
