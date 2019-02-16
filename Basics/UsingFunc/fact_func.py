0a6e3660c5278448f662b4c126afeea9fc6f0182
kajal <kajalt.23394@gmail.com>
2019-02-16 21:49:07 +0530

    Add files via upload

---
+++ Basics/UsingFunc/fact_func.py
@@ -0,0 +1,17 @@
+def factorial(fact):
+    
+    if(fact<0):
+        print('Please enter positive number.')
+    else:
+        num = 1
+        for x in range(2,fact+1):
+            num=num*x
+        return num
+    
+def main():
+    fact = eval(input('Enter the number whose factorial want to find: '))
+    result = factorial(fact)
+    print('%d!=%d'%(fact,result))
+
+if __name__ == '__main__':
+    main()
