81400f0f680f5a3e08e1c5ae817ad7c35a4bc870
kajal <kajalt.23394@gmail.com>
2019-02-16 21:05:17 +0530

    Add files via upload

---
+++ Basics/if_else.py
@@ -0,0 +1,6 @@
+#Write a program to accept no of donuts as an input, if >10 print many else print it's quantity.
+
+inp_donut = eval(input("Enter the no of donuts: "))
+if(inp_donut>10):
+    print("No. of donuts: Many")
+else:print("No. of donuts are %d"%inp_donut)
