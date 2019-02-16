8be799370e36a5957be3e4ffa7ea15b501d94e49
kajal <kajalt.23394@gmail.com>
2019-02-16 19:29:32 +0530

    Add files via upload

---
+++ Basics/basic_str_indexing.py
@@ -0,0 +1,12 @@
+#String-Immutable container=>some basics about string.
+name="kajal tingre"
+print("you entered name as: ",name)
+#'kajal tingre'
+print("Accessing 3rd char in the string(name[2]): ",name[2])
+print("2nd including to 5th excluding sub-string(name[2:5]): ",name[2:5])
+print("Printing alternate char from 1st position(name[::2]): ",name[::2])
+print("Printing string from 2nd char to end(name[2::]): ",name[2::])
+print("Print last char of string(name[-1::]): ",name[-1::])
+print("Printing in between string(name[1:-5]): ",name[1:-5])
+print("When starting index>end index, prints nothing(name[5:2]): ",name[5:2])
+print("if we give step value in above as(name[5:2:-1]): ",name[5:2:-1])
