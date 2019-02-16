8a43cfe1e0c4ee7ad2f872b27f33ae888157b567
kajal <kajalt.23394@gmail.com>
2019-02-16 22:31:25 +0530

    Add files via upload

---
+++ Basics/UsingFunc/isPrimeNo.py
@@ -0,0 +1,22 @@
+def isPrime(num):
+    if(num<2):
+        return False
+    elif(num==2):
+        return True
+    elif(num%2==0):
+        return False
+    else:
+        i=0
+        for i in range(3,int(num/2),i+2):
+            if(num%i==0):
+                return False
+        else:
+            return True
+def main():
+    num = eval(input('Enter the number to check is it prime or not: '))
+    if(isPrime(num)):
+        print('%d is prime number.'%num)
+    else:
+        print('%d is not prime number.'%num)
+if __name__=='__main__':
+    main()
