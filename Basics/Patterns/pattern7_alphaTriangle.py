18f2bf71ed1dc73329c0bbb0c7feecb4d95e3774
kajal <kajalt.23394@gmail.com>
2019-03-03 20:47:19 +0530

    Update pattern7_alphaTriangle.py

--- Basics/Patterns/pattern7_alphaTriangle.py
+++ Basics/Patterns/pattern7_alphaTriangle.py
@@ -1,3 +1,5 @@
+# Write a program to print a pattern which contains triangle of alphabets.
+
 def pattern7(n):
     ch = 65
     for i in range(1,n+1):
@@ -12,9 +14,11 @@ def pattern7(n):
                 num=num+1
         print()
         ch+=1
+        
 def main():
     n=eval(input('Enter the no of rows to print pattern: '))
     pattern7(n)
+    
 if __name__=='__main__':
     main()
         
