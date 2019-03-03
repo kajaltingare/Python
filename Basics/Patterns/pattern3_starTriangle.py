c185dfbccabbf9860cf0ed1ad4ee8bd6d3af33a0
kajal <kajalt.23394@gmail.com>
2019-03-03 20:34:10 +0530

    Update pattern3_starTriangle.py

--- Basics/Patterns/pattern3_starTriangle.py
+++ Basics/Patterns/pattern3_starTriangle.py
@@ -1,3 +1,5 @@
+# Write a program to print a pattern which contains triangle of stars.
+
 def pattern3(n):
     for i in range(1,n+1):
         for _ in range(1,n-i+1):
@@ -5,9 +7,11 @@ def pattern3(n):
         for _ in range(1,2*i):
             print('*',end='')
         print()
+        
 def main():
     n=eval(input('Enter the no of rows to print pattern: '))
     pattern3(n)
-if __name__=='__main__':
+    
+if __name__ == '__main__':
     main()
         
